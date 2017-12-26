fb7311957c149aa6f4ff1c33a979b0b4a43bd8cf
lyzs90 <leongyzs@gmail.com>
2017-12-27 00:23:41 +0800

    Make nav bar responsive

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,28 +1,72 @@
 <template>
   <v-layout>
-    <v-toolbar v-if="!displayNav" fixed app>
-      <v-avatar @click="openNav" class="cyan cursor ma-3">
-        <span class="white--text headline small-caps">{{ userInitials }}</span>
-      </v-avatar>
-    </v-toolbar>
-    <v-navigation-drawer fixed stateless dark class="blue" v-if="displayNav" :value="displayNav" v-on-clickaway="closeNav" app>
-      <v-list>
-        <v-list-tile @click="navigateTo(item.link)" v-for="item in items" :key="item.title">
-          <v-list-tile-action>
-            <v-icon dark>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <!-- Tablet / Desktop -->
+    <v-layout class="hidden-xs-only">
+      <v-toolbar v-if="!displayNav" fixed app>
+
+        <v-layout class="ml-3 mr-3">
+          <v-flex sm6>
+            <v-layout row justify-start align-center>
+              <v-avatar class="cyan ma-3">
+                <span class="white--text headline small-caps">{{ userInitials }}</span>
+              </v-avatar>
+              <v-layout @click="navigateTo(item.link)" v-for="item in items" v-if="item.showSmAndUp" :key="item.title" class="cursor pa-3 flex-initial">
+                {{ item.title }}
+              </v-layout>
+            </v-layout>
+          </v-flex>
+          <v-flex sm6>
+            <v-layout row justify-end align-center>
+              <v-btn v-if="!authenticated" @click='signUp' color='secondary'>Sign up</v-btn>
+              <v-btn v-if="!authenticated" @click='logIn' color='primary'>Log in</v-btn>
+              <v-btn v-if="authenticated" @click='logOut' color='secondary'>Log out</v-btn>
+              <span class="cursor pa-3">FAQ</span>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+
+      </v-toolbar>
+    </v-layout>
+
+    <!-- Mobile -->
+    <v-layout class="hidden-sm-and-up">
+      <v-toolbar v-if="!displayNav" fixed app>
+        <v-layout row justify-start align-center>
+          <v-flex xs4>
+            <v-avatar @click="openNav" class="cyan cursor">
+              <span class="white--text headline small-caps">{{ userInitials }}</span>
+            </v-avatar>
+          </v-flex>
+          <v-flex xs8>
+            <v-layout row justify-end align-center>
+              <v-btn v-if="!authenticated" @click='signUp' color='secondary'>Sign up</v-btn>
+              <v-btn v-if="!authenticated" @click='logIn' color='primary'>Log in</v-btn>
+              <v-btn v-if="authenticated" @click='logOut' color='secondary'>Log out</v-btn>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-toolbar>
+      <v-navigation-drawer fixed stateless dark class="blue" v-if="displayNav" :value="displayNav" v-on-clickaway="closeNav" app>
+        <v-list>
+          <v-list-tile @click="navigateTo(item.link)" v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon dark>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
   </v-layout>
 </template>
 
 <script>
 import { directive as onClickaway } from 'vue-clickaway'
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters, mapState, mapMutations } from 'vuex'
+
+import { auth } from '~/services/firebaseService'
 
 export default {
   directives: {
@@ -34,18 +78,23 @@ export default {
   data () {
     return {
       items: [
-        { icon: 'home', title: 'Home', link: '/' },
-        { icon: 'store', title: 'Marketplace', link: '/marketplace' },
-        { icon: 'view_quilt', title: 'My Policies', link: '/policies' },
-        { icon: 'help', title: 'FAQ', link: '/faq' },
-        { icon: 'settings', title: 'Settings', link: '/settings' }
+        { icon: 'home', title: 'Home', link: '/', showSmAndUp: true },
+        { icon: 'store', title: 'Buy', link: '/marketplace', showSmAndUp: true },
+        { icon: 'view_quilt', title: 'Sell', link: '/policies', showSmAndUp: true },
+        { icon: 'help', title: 'FAQ', link: '/faq', showSmAndUp: false },
+        { icon: 'settings', title: 'Settings', link: '/settings', showSmAndUp: true }
       ]
     }
   },
 
   computed: {
     ...mapGetters([
-      'userInitials'
+      'userInitials',
+      'authenticated'
+    ]),
+
+    ...mapState([
+      'user'
     ]),
 
     displayNav () {
@@ -56,11 +105,32 @@ export default {
   methods: {
     ...mapMutations({
       openNav: 'OPEN_NAV',
-      closeNav: 'CLOSE_NAV'
+      closeNav: 'CLOSE_NAV',
+      showSnackbar: 'SHOW_SNACKBAR'
     }),
 
     navigateTo (link) {
       this.$router.push(link)
+    },
+
+    logIn () {
+      this.$router.push('/login')
+    },
+
+    signUp () {
+      this.$router.push('/signup')
+    },
+
+    logOut () {
+      auth.signOut()
+        .then(() => {
+          this.showSnackbar({
+            color: 'success',
+            message: 'You have logged out!'
+          })
+
+          return this.$axios.$get('/endUserSession')
+        })
     }
   }
 }
